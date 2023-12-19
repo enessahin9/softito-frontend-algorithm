fb804f9ee8cef48b13ed123f2d5e8e7091fc06f4
Enes Şahin <enessahin9@outlook.com>
2023-12-19 17:48:22 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,67 @@
+// // document.append("h1");
+// // window.alert("asd");
+
+// const x = ["a", "b", "c"];
+// // const y = 123;
+// const y = {
+//     adres1: "xyz",
+//     ad: "Ali",
+//     soyad: "Veli",
+//     global: x
+// };
+// var a = 5;
+// let b = 6;
+// c = 10;
+// d = "Ali";
+// console.log(y);
+// x[1] = "p";
+// // x = [];
+// console.log(a, b, c, d, x, y);
+
+
+// var sonuc = 5 + 3;
+// document.writeln(sonuc);
+
+// let a = prompt();
+// document.writeln(a);
+// console.log(a);
+
+// for (let i = 0; i < 10; i++) {
+//     document.writeln(i);
+// }
+
+// var userCount = prompt("Kullanıcı sayısınız giriniz: "), total = 0
+// for (let i = 0; i < userCount; i++) {
+//     total += +prompt("Ayakkabı numarası giriniz: ")
+
+// }
+// document.writeln(total/userCount);
+
+// let a = +prompt("Kısa kenar uzunluğu giriniz:"), b = +prompt("Uzun kenar uzunluğunu giriniz:")
+// document.writeln(Math.sqrt(a*a + b**2)) // b^2
+
+
+// let sinifMevcudu = parseInt(prompt("Sınıf mevcudunu giriniz:"));
+// let toplamPuan = 0;
+
+// for (let i = 1; i <= sinifMevcudu; i++) {
+//     let vizePuani = parseInt(prompt(i + ". öğrencinin vize puanını giriniz:"));
+//     let finalPuani = parseInt(prompt(i + ". öğrencinin final puanını giriniz:"));
+
+//     let ortalama = (vizePuani * 0.4) + (finalPuani * 0.6);
+//     toplamPuan += ortalama;
+// }
+
+// let sinifOrtalamasi = toplamPuan / sinifMevcudu;
+// console.log("Sınıf puan ortalaması: " + sinifOrtalamasi);
+
+
+
+// // hoca
+// var total = 0, totalCount = +prompt("Sınıf mevcudunu giriniz:");
+// for (let i = 1; i < totalCount; i++)
+//     total += (+prompt(`${i}. Öğrencinin vize notunu giriniz:`) * 0.4) + (+prompt(`${i}. Öğrencinin final notunu giriniz:`) * 0.6)
+// document.write(`${totalCount} için ortalama not: ${total / totalCount}`)
+
+
+
